f0fc35d250bfd5f17bf977b3b94df7613c027e4c
52Hz6657 <17615704689@163.com>
2024-09-25 22:12:55 +0800

    day1

---
+++ Test.cc
@@ -0,0 +1,105 @@
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World! 你好，世界！" << endl;
+    system("pause");
+    return 0;
+}
+*/
+
+/*#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int score = 0;
+    cout << "请输入分数：" << endl;
+    cin >> score;
+    if (score >= 90)
+    {
+        cout << "优秀" << endl;
+    }
+    else if (score >= 80)
+    {
+        cout << "良好" << endl;
+    }
+    else if (score >= 70)
+    {
+        cout << "中等" << endl;
+    }
+    else
+    {
+        cout << "不及格" << endl;
+    }
+    system("pause");
+    return 0;
+}
+*/
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    double a, b, c = 0;
+    cout << "请输入三个数：" << endl;
+    cin >> a >> b >> c;
+    if (a > b)
+    {
+        if (a > c)
+        {
+            cout << "最大值为：" << a << endl;
+        }
+        else
+        {
+            cout << "最大值为：" << c << endl;
+        }
+    }
+    else
+    {
+        if (b > c)
+        {
+            cout << "最大值为：" << b << endl;
+        }
+        else
+        {
+            cout << "最大值为：" << c << endl;
+        }
+    }
+    system("pause");
+    return 0;
+}
+*/
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a, b, c;
+    cout << "请输入数字b，c：" << endl;
+    cin >> b >> c;
+    a = b > c ? b : c;
+    cout << "a的值为：" << a << endl;
+    system("pause");
+    return 0;
+}
+*/
+/*#include <iostream>
+using namespace std;
+int main()
+{
+    int a[] = {1, 2, 3, 4, 5};
+    cout << "a数组的长度为：" << sizeof(a) / sizeof(a[0]) << endl;
+    system("pause");
+    return 0;
+}
+*/
+// 冒泡排序算法
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    system("pause");
+    return 0;
+}
